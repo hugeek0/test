5facd7491d7c5c110202a93bb8614f8dafbb039e
hugeek0 <mohammadreza.yadegari2000@gmail.com>
2024-08-25 16:36:35 +0330

    test for package installing

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fixer',
+      version='1.0',
+      description='fixer is a package for get rates from web',
+      author='mhmd',
+      author_email='*****@gmail.com',
+      packages=['fixer'],
+      )
